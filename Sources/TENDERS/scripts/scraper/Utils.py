0aa8e96bec83e106c4085100698e108af69549d7
Aakash <84249722+AakashGandhi1@users.noreply.github.com>
2026-01-05 17:09:50 +0530

    fix: tender scraper

--- Sources/TENDERS/scripts/scraper/Utils.py
+++ Sources/TENDERS/scripts/scraper/Utils.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from logging.config import fileConfig
 import os
+import re
 from requests.adapters import HTTPAdapter
 import csv
 import glob
@@ -13,43 +14,39 @@ from selenium.webdriver.support import expected_conditions as EC
 import shutil
 from urllib3.util import Retry
 import time
-import pdb
-import re
 
-def sanitize_filename(filename):
-    # Remove invalid characters: \ / : * ? " < > | (on Windows)
-    sanitized = re.sub(r'[<>:"/\\|?*₹,]', '', filename)
-    # Optionally replace spaces with underscores
-    sanitized = sanitized.replace(' ', '_')
-    return sanitized
-
-MAX_RELOADS = 3
-SLEEP_TIME = 5
+MAX_RELOADS = 5
+SLEEP_TIME = 30
 class SeleniumScrappingUtils(object):
     def __init__(self):
         pass
-        
+
+
+    def sanitize_filename(filename):
+        # Remove invalid characters and replace spaces with underscores
+        sanitized = re.sub(r'[<>:"/\\|?*]', '', filename)
+        sanitized = sanitized.replace(' ', '_')
+        # Truncate filename if it's too long (Windows has a 255-character limit)
+        return sanitized[:255]
+
+
+
     def get_tender_id(path):
         dataframe = pd.read_csv(path)
         tender_id = dataframe["tender.id"][dataframe['tender.stage'] == "AOC"]
         return tender_id
     
-    def get_multiple_page_elements(browser,xpath = None):
-        '''
-        returns list of page element identifies with the given path
-        '''
-        page_element = WebDriverWait(browser, SLEEP_TIME).until(EC.presence_of_all_elements_located((By.XPATH,xpath)))
+    def get_multiple_page_elements(driver,xpath = None):
+
+        page_element = WebDriverWait(driver, SLEEP_TIME).until(EC.presence_of_all_elements_located((By.XPATH,xpath)))
         return page_element
     
-    def get_page_element(browser, xpath=None):
-        ''' Get page element by xpath
-        '''
-        page_element = WebDriverWait(browser, SLEEP_TIME).until(EC.presence_of_element_located((By.XPATH,xpath)))
+    def get_page_element(driver, xpath=None):
+
+        page_element = WebDriverWait(driver, SLEEP_TIME).until(EC.presence_of_element_located((By.XPATH,xpath)))
         return page_element
-    def input_text_box(browser, select_element, text=None):
-        '''
-        Input text in input box
-        '''
+    def input_text_box(driver, select_element, text=None):
+
         select_element.send_keys(text)
     def save_image_as_png(image_element):
         '''
@@ -57,52 +54,42 @@ class SeleniumScrappingUtils(object):
         '''
         with open('captcha_image.png', 'wb') as file:
              file.write(image_element.screenshot_as_png)
+
     def get_text_from_element(element):
-        ''' Extracts name from webelement'''
+       
         name_of_element = [element[i].text for i in range(len(element))]
         return name_of_element
-    def extract_vertical_table(table_section,name_of_file,skip_header_number = None):
-        '''
-        Extracts vertical tables
-        '''
-        with open(str(name_of_file)+".csv", 'w', newline='', encoding="utf-8") as csvfile:
-            wr = csv.writer(csvfile)
-            for row in table_section.find_elements(By.CSS_SELECTOR,'tr')[skip_header_number:]:
-                wr.writerow([d.text for d in row.find_elements(By.CSS_SELECTOR,'td')])
-    def extract_horizontal_table(table_section,name_of_file,skip_header_number = None):
-        '''
-        Extracts horizontal tables
-        '''
-        sanitized_name_of_file = sanitize_filename(name_of_file)
 
-        with open(str(sanitized_name_of_file) + ".csv", 'w', newline='', encoding="utf-8") as csvfile:
+    def extract_vertical_table(table_section, name_of_file, skip_header_number=None):
+
+        # Sanitize the filename
+        safe_filename = SeleniumScrappingUtils.sanitize_filename(str(name_of_file))
+
+        with open(safe_filename + ".csv", 'w', newline='', encoding='utf-8') as csvfile:
             wr = csv.writer(csvfile)
-            for row in table_section.find_elements(By.CSS_SELECTOR,"tbody"):
-                try:
-                     wr.writerow([d.text for d in row.find_elements(By.CSS_SELECTOR,'td:nth-of-type(2n+1)')[skip_header_number:]])
-                except Exception as e:
-                    #pdb.set_trace()    
-                    print(f"Error processing row: {e}")
-                    continue
-                wr.writerow([d.text for d in row.find_elements(By.CSS_SELECTOR,'td:nth-of-type(2n+2)')])
+            for row in table_section.find_elements(By.CSS_SELECTOR, 'tr')[skip_header_number:]:
+                wr.writerow([d.text for d in row.find_elements(By.CSS_SELECTOR, 'td')])
+    def extract_horizontal_table(table_section, name_of_file, skip_header_number=None):
+
+        # Sanitize the filename
+        safe_filename = SeleniumScrappingUtils.sanitize_filename(str(name_of_file))
+
+        with open(safe_filename + ".csv", 'w', newline='', encoding='utf-8') as csvfile:
+            wr = csv.writer(csvfile)
+            for row in table_section.find_elements(By.CSS_SELECTOR, "tbody"):
+                wr.writerow(
+                    [d.text for d in row.find_elements(By.CSS_SELECTOR, 'td:nth-of-type(2n+1)')[skip_header_number:]])
+                wr.writerow([d.text for d in row.find_elements(By.CSS_SELECTOR, 'td:nth-of-type(2n+2)')])
                 
-            
-            
-                
-    def concatinate_csvs(path_to_save,name_of_file, tender_status):
-        '''
-        combines all the csvs
-        '''
+    def concatinate_csvs(path_to_save,name_of_file):
+
         extension = 'csv'
         all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
         combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ], axis = 1)
-        combined_csv['Tender Stage'] = tender_status
         combined_csv.to_csv(path_to_save + name_of_file+".csv", index=False, encoding='utf-8-sig')
    
     def remove_csvs(directory):
-        '''
-        removes all the csvs
-        '''
+
         files_in_directory = os.listdir(directory)
         filtered_files = [file for file in files_in_directory if file.endswith(".csv")]
         for file in filtered_files:
@@ -121,6 +108,6 @@ class SeleniumScrappingUtils(object):
             print("File found")
             return True
     
-    def select_drop_down(browser,id,value):
-         selected_element = Select(browser.find_element("xpath",id))
+    def select_drop_down(driver,id,value):
+         selected_element = Select(driver.find_element("xpath",id))
          selected_element.select_by_value(value)
